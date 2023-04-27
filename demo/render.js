484b8b4b832ad3b53ad0e3bf4a7ac13c96239c80
yoannchb-pro <71560747+yoannchb-pro@users.noreply.github.com>
2023-04-26 23:27:42 -0400

    demo

--- demo/render.js
+++ demo/render.js
@@ -2,8 +2,18 @@ const editor = document.querySelector("#editor");
 const data = document.querySelector("#data");
 const iframe = document.querySelector("#view");
 
-const defaultTemplate = "<h1>{%= gretting %}</h1>";
-const defaultDatas = { gretting: "Hello World!" };
+const defaultTemplate = `<h1>{%= gretting %}</h1>
+<p>It remain {%= todos.length %} items in my todo list:</p>
+<ul>
+    {% for(const todo of todos) { %}
+        <li>{%= todo %}</li>
+    {% } %}
+</ul>
+`;
+const defaultDatas = {
+  gretting: "Welcome to my todo list!",
+  todos: ["Understand tempjs", "Learn typescript", "Make a cake"],
+};
 const defaultRender = tempjs.compile(defaultTemplate, defaultDatas);
 
 editor.value = defaultTemplate;
